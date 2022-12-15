5ffc227c8013dce5adb3224d3c3474d47b427340
ravi39094 <ravikiran.marella@tietoevry.com>
2022-12-15 23:37:23 +0530

    Update kubesec-scan.sh

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -20,9 +20,10 @@ kubesec ()
       --data-binary=@"${FILE}" \
       https://v2.kubesec.io/scan
 }
-scan_result=kubesec "$k8s_deployment_service.yaml"
-scan_result=kubesec "$./k8s_deployment_service.yaml"  | jq .[0].message -r
-scan_score=kubesec $"k8s_deployment_service.yaml" | jq .[0].score 
+scan_result=kubesec "k8s_deployment_service.yaml"
+scan_result=kubesec "./k8s_deployment_service.yaml"  | jq .[0].message -r
+scan_score=kubesec k8s_deployment_service.yaml | jq .[0].score 
+scan_score=kubesec ./k8s_deployment_service.yaml | jq .[0].score 
 # using kubesec docker image for scanning
 # scan_result=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml)
 # scan_message=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml | jq .[].message -r)
